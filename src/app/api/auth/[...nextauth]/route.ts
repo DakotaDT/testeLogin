cee3af9460a8bcffbf66872a39c8d2f5be961646
DakotaDT <dakotadt2@hotmail.com>
2024-04-30 12:02:31 -0300

    funcionapourra

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
 import { authOptions } from "@/app/lib/auth";
-import { Account, User as AuthUser, NextAuthOptions } from "next-auth";
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
