67aa4017a4bebe39f5aae512660afb166df88bf9
DakotaDT <dakotadt2@hotmail.com>
2024-04-30 11:46:35 -0300

    secone

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,17 @@
 import NextAuth from "next-auth/next";
-import { Account, User as AuthUser } from "next-auth";
+import { Account, User as AuthUser, NextAuthOptions } from "next-auth";
 import bcrypt from "bcryptjs"
 import Github from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 import User from "@/models/User";
 import connect from "@/utils/db";
 
-export const authOptions: any = {
+export const authOptions: NextAuthOptions = {
     // Configure one or more authentication providers
     providers: [
         CredentialsProvider({
-            id: "credentials",
-            name: "Credentials",
+            id: "credentials" as string,
+            name: "Credentials" as string,
             credentials: {
                 email: { label: "Email", type: "text" },
                 password: { label: "Password", type: "password" },
